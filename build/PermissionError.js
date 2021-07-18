8070022bf60a98e94fe58edf3032513b918e83bc
Arthur <55070374+xavier-arthur@users.noreply.github.com>
2021-07-18 15:34:52 -0300

    Delete build directory

--- build/PermissionError.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class PermissionError extends Error {
-    constructor(msg = '') {
-        super(msg);
-        this.name = 'PermissionError';
-    }
-}
-exports.default = PermissionError;
--- build/XP_Discord.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.retrieve = void 0;
-function retrieve(charName) {
-}
-exports.retrieve = retrieve;
